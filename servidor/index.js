8768600aefbd846c340e7947c24dd2fe50d304f5
Ceci-Caccamo <67034112+Ceci-Caccamo@users.noreply.github.com>
2020-12-29 13:11:52 -0300

    Validacion con JWT

---
+++ servidor/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const conectarDB = require("./config/db");
+
+//crear el servidor
+const app = express();
+
+//conectar a la base de datos
+conectarDB();
+
+//Habilitar express.json
+app.use(express.json({extended:true}))
+
+//Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//Importar Rutas
+app.use('/api/usuarios', require('./routes/usuarios'))
+
+//Definir la pagina Principal
+/* app.get('/', (req, res)=>{
+    res.send('Hola Mundo')
+}); */
+
+//arrancar la app
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
